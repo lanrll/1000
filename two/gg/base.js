ff71b9234dca76238d9a7dbd297e446dc2d30609
lanrll <53162652+lanrll@users.noreply.github.com>
2019-08-02 20:50:36 +0800

    201907022050

--- two/gg/base.js
+++ two/gg/base.js
@@ -1,3 +1,38 @@
+//判断当前浏览器类型
+function isBrowser() {
+    var userAgent = navigator.userAgent;
+    //微信内置浏览器
+    if(userAgent.match(/MicroMessenger/i) == 'MicroMessenger') {
+        return "MicroMessenger";
+    }
+    //QQ内置浏览器
+    else if(userAgent.match(/QQ/i) == 'QQ') {
+        return "QQ";
+    }
+    //Chrome
+    else if(userAgent.match(/Chrome/i) == 'Chrome') {
+        return "Chrome";
+    }
+    //Opera
+    else if(userAgent.match(/Opera/i) == 'Opera') {
+        return "Opera";
+    }
+    //Firefox
+    else if(userAgent.match(/Firefox/i) == 'Firefox') {
+        return "Firefox";
+    }
+    //Safari
+    else if(userAgent.match(/Safari/i) == 'Safari') {
+        return "Safari";
+    }
+    //IE
+    else if(!!window.ActiveXObject || "ActiveXObject" in window) {
+        return "IE";
+    }
+    else {
+        return "未定义:"+userAgent;
+    }
+}
 //生产min-max的随机数
 function randomNum( min, max){
     switch(arguments.length){
@@ -12,3 +47,11 @@ function randomNum( min, max){
             break;
     }
 }
+// 封装一个获取外部样式的方法
+function getStyle (obj, attr) {
+    if (obj.currentStyle) {
+      return obj.currentStyle[attr]
+    } else {
+      return getComputedStyle(obj, false)[attr]
+    }
+  }
