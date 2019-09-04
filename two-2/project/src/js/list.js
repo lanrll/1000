cd1c13249a6f3c749c2fbf11b3507ed921c23a9e
lanrll <53162652+lanrll@users.noreply.github.com>
2019-09-04 23:45:26 +0800

    201909042345

--- two-2/project/src/js/list.js
+++ two-2/project/src/js/list.js
@@ -1,11 +1,23 @@
 require(['./config'], () => {
-  require(['indexHeader', 'indexProduct','footer'], () => {
+  require(['template','url','indexHeader', 'indexProduct','footer'], (template,url) => {
     class cart{
       constructor(){
-
+        this.getProductShowData().then(resp => {
+          console.log(resp)
+          if(resp.code === 200){
+            $('#productShow').html(template('officeProductShow', {...resp}))
+          }
+        })
       }
       init(){
 
+      }
+      getProductShowData(){
+        return new Promise(resolve => {
+          $.get(`${url.rapBaseUrl}/shopList`, resp => {
+            resolve(resp)
+          })
+        })
       }
     }
     return new cart();
