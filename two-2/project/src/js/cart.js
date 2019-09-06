e4edc946773f41d164bd0263d679fcc36830502c
lanrll <53162652+lanrll@users.noreply.github.com>
2019-09-07 00:19:57 +0800

    201909070019

--- two-2/project/src/js/cart.js
+++ two-2/project/src/js/cart.js
@@ -2,20 +2,11 @@ require(['./config'], () => {
   require(['template','indexHeader', 'footer'], (template) => {
     class cart{
       constructor(){
-        // let carts = localStorage.getItem('cart');
-        // this.cart = JSON.parse(carts)
-        // this.num = 0;
-        // var linum = 0 ;
-        // this.cart.forEach( shop => {
-        //   linum += shop.check ? +1 : -1;
-        // })
-        // this.num = linum;
-        // console.log(this.num)
         this.init();
-        this.deltelProduct();
-        this.checksChange();
-        this.allCheckChange();
-        // this.num = 0;
+        // this.deleteProduct();
+        // this.checksChange();
+        // this.allCheckChange();
+        // this.productNumberChange();
       }
       init(){
         let cart = localStorage.getItem('cart')
@@ -29,50 +20,48 @@ require(['./config'], () => {
             linum += shop.check ? +1 : -1;
           })
           this.num = linum;
-          console.log(this.num)
           num = this.cart.reduce((res, shop) => {
             res += shop.num;
             return res;
           }, 0)
           $('#productNum').html(num);
           if(this.num == this.cart.length){
-            $('#allCheck').prop('checked', true)
+            if(this.num === 0){
+              $('#allCheck').prop('checked', false)
+            }else{
+              $('#allCheck').prop('checked', true)
+            }
           }else{
             $('#allCheck').prop('checked', false)
           }
           let str = template('productShowOne', {cart: this.cart})
           $('.singleProductShow').html(str)
-          this.deltelProduct();
           this.checksChange();
           this.allCheckChange();
           this.productPriceTotal();
+          this.productNumberChange();
+          this.deleteProduct();
         }
       }
 
       checksChange(){
         const _this = this;
         $('.checks').on('change', function(){
-          const id = Number($(this).parents('.productSuowOne').attr('data-id'))
-          _this.cart = JSON.parse(localStorage.getItem('cart'))
+          const id = Number($(this).parents('.productShowOne').attr('data-id'))
           _this.cart = _this.cart.map(shop => {
             if(shop.id === id) shop.check = $(this).prop('checked')
             return shop;
           })
           _this.num += $(this).prop('checked') ? +1 : -1;
           $('#allCheck').prop('checked', _this.num === _this.cart.length)
-         console.log(_this.num)
           localStorage.setItem('cart', JSON.stringify(_this.cart));
-          _this.productPriceTotal();
+          _this.init();
         })
       }
 
       allCheckChange(){
         const _this = this;
         $('#allCheck').on('change', function(){
-          let cart = localStorage.getItem('cart')
-          if(cart){
-            _this.cart = JSON.parse(cart)
-            console.log(_this.cart)
             _this.cart.forEach(shop => {
               shop.check = $(this).prop('checked')
             })
@@ -80,17 +69,44 @@ require(['./config'], () => {
             _this.num = $(this).prop('checked') ? _this.cart.length : 0;
             localStorage.setItem('cart', JSON.stringify(_this.cart));
             _this.productPriceTotal();
-          }
         })
       }
 
-      deltelProduct(){
+      deleteProduct(){
         const _this = this;
         $('.deleteProduct').on('click', function() {
           _this.cart = JSON.parse(localStorage.getItem('cart'))
-          const id = Number($(this).parents('.productSuowOne').attr('data-id'));
+          const id = Number($(this).parents('.productShowOne').attr('data-id'));
           _this.cart = _this.cart.filter(shop => {
             return shop.id != id ;
+          })
+          localStorage.setItem('cart', JSON.stringify(_this.cart))
+          _this.init();
+        })
+      }
+
+      productNumberChange(){
+        const _this = this;
+        $('.reduce').on('click', function(){
+          let id = Number($(this).parents('.productShowOne').attr('data-id'));
+          _this.cart = _this.cart.map(shop => {
+            if(shop.id === id) {
+              if(shop.num !=1){
+                shop.num--
+              }else{
+                shop.num = 1;
+              }
+            }
+            return shop;
+          })
+          localStorage.setItem('cart', JSON.stringify(_this.cart))
+          _this.init();
+        })
+        $('.increase').on('click', function(){
+          let id = Number($(this).parents('.productShowOne').attr('data-id'));
+          _this.cart = _this.cart.map(shop => {
+            if(shop.id === id) shop.num++
+            return shop;
           })
           localStorage.setItem('cart', JSON.stringify(_this.cart))
           _this.init();
