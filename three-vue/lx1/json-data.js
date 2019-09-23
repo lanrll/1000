9786c39f4dcdaafd1e0c0eca0110f5ad70660619
zyqlanrll <1020722526@qq.com>
2019-09-23 19:51:07 +0800

    201909231951

---
+++ three-vue/lx1/json-data.js
@@ -0,0 +1,66 @@
+var dzData = {
+    "total": 695,
+    "size": 12,
+    "current": 1,
+    "records": [{
+        "id": "magzne944d3da097f84ad983da5fd6ae20fd56",
+        "cover": "https://reader-statics.duzhe.com/fa705cb8-ce11-4b04-83e2-3d0c48cf0c4c.jpg?x-oss-process=image/resize,m_fixed,h_669,w_480",
+        "yearPeriod": "2019年 第19期",
+        "sortDt": "2019-10-01 00:00:00"
+    }, {
+        "id": "magzne13f077d4475a4bf68c9c5c2a9754238f",
+        "cover": "https://reader-statics.duzhe.com/a07329d7-312d-4f0c-b28b-4eff430b2f0a.jpg?x-oss-process=image/resize,m_fixed,h_669,w_480",
+        "yearPeriod": "2019年 第18期",
+        "sortDt": "2019-09-15 00:00:00"
+    }, {
+        "id": "magzne75982df0256a4adaa2ed0b0fca803f0f",
+        "cover": "https://reader-statics.duzhe.com/df32d63e-1b63-41ec-bf5e-3d4ca7a48ba8.jpg?x-oss-process=image/resize,m_fixed,h_669,w_480",
+        "yearPeriod": "2019年 第17期",
+        "sortDt": "2019-09-01 00:00:00"
+    }, {
+        "id": "magzne5b89e9465a24475eb88dd2fc4828cf47",
+        "cover": "https://reader-statics.duzhe.com/bbd45b6b-da1c-4f05-a559-932dc6082eb9.jpg?x-oss-process=image/resize,m_fixed,h_669,w_480",
+        "yearPeriod": "2019年 第16期",
+        "sortDt": "2019-08-16 00:00:00"
+    }, {
+        "id": "magzned1e79fa675fc45679dfdc2f1e0515ef0",
+        "cover": "https://reader-statics.duzhe.com/be463e04-b378-4f8a-b035-dcf09b454e0c.jpg?x-oss-process=image/resize,m_fixed,h_669,w_480",
+        "yearPeriod": "2019年 第15期",
+        "sortDt": "2019-08-01 00:00:00"
+    }, {
+        "id": "magzne994bad6f3eef495b890de29dee648fae",
+        "cover": "https://reader-statics.duzhe.com/29dbc659-770c-47d6-98e7-dd237fb27fce.jpg?x-oss-process=image/resize,m_fixed,h_669,w_480",
+        "yearPeriod": "2019年 第14期",
+        "sortDt": "2019-07-15 00:00:00"
+    }, {
+        "id": "magznefb03a8d628974a58848d6f93cc3626a8",
+        "cover": "https://reader-statics.duzhe.com/16068421-f8c0-4251-b3c7-83ecefe6c179.jpg?x-oss-process=image/resize,m_fixed,h_669,w_480",
+        "yearPeriod": "2019年 第13期",
+        "sortDt": "2019-07-01 00:00:00"
+    }, {
+        "id": "magzneb82a6bdeb2514fdfbe6259d28b029abd",
+        "cover": "https://reader-statics.duzhe.com/74ebfac1-ac82-4447-82f9-400ed7fff0cd.jpg?x-oss-process=image/resize,m_fixed,h_669,w_480",
+        "yearPeriod": "2019年 第12期",
+        "sortDt": "2019-06-16 00:00:00"
+    }, {
+        "id": "magzne2b795bd678fe43f1a27edecd5a2c5680",
+        "cover": "https://reader-statics.duzhe.com/023ef407-9906-43a7-9e1b-42a61eb6de4d.jpg?x-oss-process=image/resize,m_fixed,h_669,w_480",
+        "yearPeriod": "2019年 第11期",
+        "sortDt": "2019-05-17 16:55:42"
+    }, {
+        "id": "magzne6faea1b92c9d4abd8b87efb39bed4e52",
+        "cover": "https://reader-statics.duzhe.com/af28ebf9-91ab-4827-9440-ff0af00dc4d1.jpg?x-oss-process=image/resize,m_fixed,h_669,w_480",
+        "yearPeriod": "2019年 第10期",
+        "sortDt": "2019-05-17 12:52:45"
+    }, {
+        "id": "magzne01806a716d70421e9c89547770aec466",
+        "cover": "https://reader-statics.duzhe.com/143bb5fa-6386-4c37-906b-2eeeb4f16f8c.jpg?x-oss-process=image/resize,m_fixed,h_669,w_480",
+        "yearPeriod": "2019年 第9期",
+        "sortDt": "2019-05-16 17:05:28"
+    }, {
+        "id": "magzned042de94cc964ba08bc08221eb50015d",
+        "cover": "https://reader-statics.duzhe.com/217fa1f9-f7ca-4a57-83b3-9f562b6d6635.jpg?x-oss-process=image/resize,m_fixed,h_669,w_480",
+        "yearPeriod": "2019年 第8期",
+        "sortDt": "2019-05-16 11:32:05"
+    }]
+}
